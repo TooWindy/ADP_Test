c7d5e7c596c8faa3247259d683d6d869fb8e68e0
Chris E <14ceng@gmail.com>
2022-04-21 13:54:38 -0400

    Added some comments to code

--- test.js
+++ test.js
@@ -2,9 +2,11 @@ const axios = require('axios')
 
 async function completeTask() {
   try {
+    //First call to get our task
     const task =  await axios.get('https://interview.adpeai.com/api/v1/get-task')
     const taskData = task.data
 
+    //All operations are completed using this table as a reference
     const operationTable = {
       addition : taskData.left + taskData.right,
       subtraction: taskData.left - taskData.right,
@@ -12,7 +14,7 @@ async function completeTask() {
       division: taskData.left / taskData.right,
       remainder: taskData.left % taskData.right
     }
-
+    //Second call to send our results
     const response = await axios.post('https://interview.adpeai.com/api/v1/submit-task', {
       id: taskData.id,
       result: operationTable[taskData.operation]
@@ -23,6 +25,7 @@ async function completeTask() {
     console.log(`Status code ${response.status}: ${response.data}`)
   }
   catch(err) {
+    //Any errors are logged to the console (error code & the associated message recieved from the endpoint)
     console.log(`Error code ${err.response.status}: ${err.response.data}`)
   }
 }
