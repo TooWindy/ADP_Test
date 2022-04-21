4b1351f447e588e3e32e7ca7ccbbb1ddded78cb3
Chris E <14ceng@gmail.com>
2022-04-21 13:24:15 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+
+async function getTask() {
+  try {
+    const task =  await axios.get('https://interview.adpeai.com/api/v1/get-task')
+    const taskData = task.data
+
+    const operationTable = {
+      addition : taskData.left + taskData.right,
+      subtraction: taskData.left - taskData.right,
+      multiplication: taskData.left * taskData.right,
+      division: taskData.left / taskData.right,
+      remainder: taskData.left % taskData.right
+    }
+
+    const response = await axios.post('https://interview.adpeai.com/api/v1/submit-task', {
+      id: taskData.id,
+      result: operationTable[taskData.operation]
+    })
+
+    console.log(`Operation: ${taskData.operation}`)
+    console.log(`Result: ${operationTable[taskData.operation]}`)
+    console.log(`Status code ${response.status}: ${response.data}`)
+  }
+  catch(err) {
+    console.log(`Error code ${err.response.status}: ${err.response.data}`)
+  }
+}
+
+
+console.log(getTask())
